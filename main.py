080cf393255091e7bac176a0aa657ac79fe4895b
blackandcold <j.kurz@gmx.at>
2017-09-19 09:50:12 +0200

    added a minimization algorithm and played around with different ones, fixed on fmin_powell

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from matplotlib.pyplot import plot, xlabel, ylabel, show
 import HFSolver
 import numpy as np
 from pylab import *
+from scipy.optimize import fmin_powell
 
 # Fixed values that do not change while running the program
 dt = np.float
@@ -11,7 +12,7 @@ nucleiPositions = [(-1), (-1), (-1), (-1), (1), (1), (1), (1)]
 base = [(13.00773), (1.962079), (0.444529), (0.1219492)]*2
 numberOfElectrons = 2
 breakValue = 1e-8
-maximumIterations = 400 # Hard bound for the SCL, to not lock my computer up if it diverges!
+maximumIterations = 200 # Hard bound for the SCL, to not lock my computer up if it diverges!
 
 hfSolver = HFSolver.HFSolver(base, nucleiPositions, numberOfElectrons, breakValue, maximumIterations)
 
@@ -33,3 +34,15 @@ plot(rRange, Er, 'r--')
 xlabel('R_AB')
 ylabel('Energy')
 show()
+
+# find minimal distance
+
+def optimizationFunction(r_ab, rho):
+    return hfSolver.SCF(rho, r_ab)[0]
+
+
+#  List of minimization algorithms: https://docs.scipy.org/doc/scipy-0.10.1/reference/tutorial/optimize.html
+#  fmin_powell needs a function to minimize that returns one value
+R_AB_min = fmin_powell(optimizationFunction, 1., args=(rho,))
+print 'Minimal radius: ', R_AB_min
+(energy, rho) = hfSolver.SCF(rho, R_AB_min)
